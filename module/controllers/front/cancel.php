104306b8f5a2681641a800c031c28e40bd7fb6a4
MarekW <marekw@tiltup.io>
2023-08-16 15:32:12 +0200

    Redirect to order history

--- module/controllers/front/cancel.php
+++ module/controllers/front/cancel.php
@@ -7,12 +7,18 @@ class TiltUpCryptoPaymentsModuleCancelModuleFrontController extends TiltUpFrontC
     public function initContent()
     {
         parent::initContent();
-
-        $this->setTemplate('module:tiltupcryptopaymentsmodule/views/templates/front/cancel.tpl');
     }
 
     protected function handleRequest(Order $order)
     {
         $this->updateOrderState($order, (int)Configuration::getGlobalValue(TiltUpCryptoPaymentsModule::CRYPTO_PAYMENT_CANCELLED_STATUS_CONFIG));
+        $customer = new Customer($order->id_customer);
+
+        Tools::redirect(
+            $this->context->link->getPageLink('guest-tracking', null, null, [
+                'order_reference' => $order->reference,
+                'email' => $customer->email
+            ])
+        );
     }
 }
