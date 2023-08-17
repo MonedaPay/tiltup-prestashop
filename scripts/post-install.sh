d7b89bd9356b434f3e9055c129d25fb153db26f6
MarekW <marekw@tiltup.io>
2023-08-17 10:59:02 +0200

    Config script

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -3,5 +3,7 @@
 php -d memory_limit=256M
 echo "Installing Tiltup Crypto Payments Module"
 php bin/console prestashop:module install tiltupcryptopaymentsmodule
+echo "Configuring Tiltup Crypto Payments Module"
+php bin/console prestashop:module configure tiltupcryptopaymentsmodule
 rm -rf /var/www/html/var/cache/dev
 rm -rf /var/www/html/var/logs
