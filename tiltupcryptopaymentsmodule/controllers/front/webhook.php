984c246feb5a80c8d1c6c4f555e30aaf6ccfffe8
MarekW <marekw@tiltup.io>
2023-08-18 16:09:35 +0200

    Updated copyright

--- tiltupcryptopaymentsmodule/controllers/front/webhook.php
+++ tiltupcryptopaymentsmodule/controllers/front/webhook.php
@@ -1,23 +1,18 @@
 <?php
 /**
+ * TiltUp_TiltUpCryptoPaymentsModule extension
  *
-  * Copyright since 2023 TiltUp Sp z o. o.
-  *
-   NOTICE OF LICENSE
-  *
-  * This source file is subject to the Academic Free License version 3.0
-  * that is bundled with this package in the file LICENSE.md.
-  * It is also available through the world-wide-web at this URL:
-  * https://opensource.org/licenses/AFL-3.0
-  * If you did not receive a copy of the license and are unable to
-  * obtain it through the world-wide-web, please send an email
-  * to license@prestashop.com so we can send you a copy immediately.
-  *
-  * @author  TiltUp Sp. z o.o
-  * @copyright Since 2023 TiltUp Sp z o. o.
-  * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License version 3.0
-
- */
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the GNU Lesser General Public License
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://www.gnu.org/licenses/lgpl-3.0.en.html
+ *
+ * @author         TiltUp Sp. z o. o.
+ * @copyright      Copyright (c) 2023-2031
+ * @license        https://www.gnu.org/licenses/lgpl-3.0.en.html GNU Lesser General Public License
+*/
 require_once __DIR__ . '/TiltUpFrontWebhookController.php';
 require_once __DIR__ . '/../../TiltUpAggregatedOrderStatus.php';
 require_once __DIR__ . '/../../TiltUpSimpleLogger.php';
